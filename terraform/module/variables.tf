0481d613ece062d18fd227ce2f799adfef0a2552
satakumara <sat.bca@gmail.com>
2022-07-21 08:14:33 +0530

    firsttest

---
+++ terraform/module/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  description = "The region information"
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+}
+
+//Networking
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+
+  description = "The CIDR block for the public subnet"
+}
+
+variable "private_subnets_cidr" {
+
+  description = "The CIDR block for the private subnet"
+}
+variable "availability_zones"{
+  description = "Availability zones"
+}
